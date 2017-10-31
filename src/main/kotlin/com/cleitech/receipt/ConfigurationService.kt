ba762a0207f754f1c92021c75aef314852a985a7
dev <itinera-support@famifed.be>
2017-10-31 15:43:36 +0100

    Another Commit. Compile and run. Move A drive file from a directory to another directory

--- src/main/kotlin/com/cleitech/receipt/ConfigurationService.kt
+++ src/main/kotlin/com/cleitech/receipt/ConfigurationService.kt
@@ -1,22 +1,38 @@
 package com.cleitech.receipt
 
+import com.beust.klaxon.JsonArray
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Parser
+import com.beust.klaxon.string
 import org.springframework.stereotype.Component
 
 
 data class DriveToOCREntry(
-        val toScanDirId : String,
-        val uploadedDirId : String,
-        val ocrCategory : String
+        val toScanDirId: String,
+        val uploadedDirId: String,
+        val ocrCategory: String
 )
 
 @Component
 class ConfigurationService {
-    fun driveToOcrEntries() : List<DriveToOCREntry>
-            = listOf(
+
+    private fun parse(name: String): Any? {
+        val cls = Parser::class.java
+        return cls.getResourceAsStream(name)?.let { inputStream ->
+            return Parser().parse(inputStream)
+        }
+    }
+
+    fun driveToOcrEntries(): List<DriveToOCREntry> {
+        val obj = parse("driveToOcr.json") as JsonArray<JsonObject>
+        return obj.map {
             DriveToOCREntry(
-                    "0B9I1Fu1LKhuoSnloT2hCVng0UUE",
-                    "0B9I1Fu1LKhuoXzBlbGFBWlFPMDg",
-                    "toSentDummy"
-                    )
-    )
+                    it.string("toScanDirId")!!,
+                    it.string("uploadedDirId")!!,
+                    it.string("ocrCategory")!!
+            )
+        }
+
+    }
+
 }
